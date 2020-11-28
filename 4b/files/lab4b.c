f8547c07d0d9b9cb4c6a29498771c8945ec27fe9
William Randall <wrandall1000@gmail.com>
2020-11-28 17:08:46 -0600

    Update lab4b.c

--- 4b/files/lab4b.c
+++ 4b/files/lab4b.c
@@ -2,3 +2,22 @@
 // EMAIL: wrandall1000@gmail.com
 // ID: 805167986
 
+#include <mraa.h> //aio and gpio
+#include <stdio.h> //fprintf
+#include <stdlib.h> //exit // rand
+#include <string.h> //strerror //atoi
+#include <getopt.h> //getopt
+#include <time.h> //timespec //clock_gettime
+#include <poll.h> //poll
+#include <fcntl.h> //open //creat
+#include <unistd.h> //close //fork //pipe
+#include <math.h> //
+
+
+#define SYMBOL value
+#define ... ...
+
+
+#define BUTTON_PIN 60
+#define TEMP_PIN 1
+
