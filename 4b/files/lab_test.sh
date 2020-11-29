39d8b2ffc5e74ca8bdba5ba9d4609706eb00146f
William Randall <wrandall1000@gmail.com>
2020-11-29 03:12:35 -0600

    update makefile and check script

--- 4b/files/lab_test.sh
+++ 4b/files/lab_test.sh
@@ -1,53 +1,27 @@
-# #!/bin/bash
-
-# ./lab4b --log=test_output --scale=C & 
-# pid=$!
-# sleep 10
-# kill $pid
-
-# output=$(cat test_output)
-# rm -rf test_output
-
-# pass="PASS"
-# #regex for each line
-# #08:39:17 71.1
-# regex="^\d{2}:\d{2}:\d{2} \d+\.\d{1}$"
-
-# for line in $(output)
-# do
-# 	if [[ $line =~  $regex]]
-# 	then
-# 		#is true
-# 	else
-# 		#failed
-# 		pass="FAILED"
-# 	fi
-# done
-
-# echo "$pass"
-
 #!/bin/bash
 
-./lab4b --period=2 --scale=C --log=smoketest_res &
+./lab4b --log=test_output --scale=C & 
 pid=$!
-sleep 5
+sleep 10
 kill $pid
 
-tr " " "_" < smoketest_res > smoketest_result
-rm smoketest_res
-test=$(cat smoketest_result)
-rm smoketest_result
+output=$(cat test_output)
+rm -rf test_output
 
-result="Passes smoketest"
-expected="[0-9]{2}:[0-9]{2}:[0-9]{2}_[0-9]+\.[0-9]$"
-for line in $test
+pass="PASS"
+#regex for each line
+#08:39:17 71.1
+regex="^\d{2}:\d{2}:\d{2} \d+\.\d{1}$"
+
+for line in $(output)
 do
-	if [[ $line =~ $expected ]]
+	if [[ $line =~  $regex]]
 	then
-		continue
+		#is true
 	else
-		result="Fails smoketest"
+		#failed
+		pass="FAILED"
 	fi
 done
 
-echo "$result"
+echo "$pass"
