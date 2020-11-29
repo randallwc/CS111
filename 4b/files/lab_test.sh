000909780a01e4c4a946c857bbba987cd480d436
William Randall <wrandall1000@gmail.com>
2020-11-29 03:03:28 -0600

    update lab_test.sh and README

--- 4b/files/lab_test.sh
+++ 4b/files/lab_test.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+./lab4b --log=test_output --scale=C & pid=$!
+sleep 10
+kill $pid
+
+output=$(cat test_output)
+rm -rf test_output
+
+pass="PASS"
+#regex for each line
+#08:39:17 71.1
+regex="^\d{2}:\d{2}:\d{2} \d+\.\d{1}$"
+
+for line in $(output)
+do
+	if [[ $line =~  $regex]]
+	then
+		#is true
+	else
+		#failed
+		pass="FAILED"
+	fi
+done
+
+echo "$pass"
