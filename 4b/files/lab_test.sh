52ff662a51eb06cd893002469f87b8e8844f791b
William Randall <wrandall1000@gmail.com>
2020-11-29 03:35:26 -0600

    Update lab_test.sh

--- 4b/files/lab_test.sh
+++ 4b/files/lab_test.sh
@@ -1,54 +1,29 @@
 #!/bin/bash
-# ./lab4b --log=test_output --scale=C & 
-# pid=$!
-# sleep 5
-# kill $pid
-
-# tr " " "-" < test_output > translated_out
-# output=$(cat translated_out)
-# rm -rf test_output
-# rm -rf translated_out
-
-# pass="PASS"
-# #regex for each line
-# #08:39:17 71.1
-# regex="^[0-9]{2}:[0-9]{2}:[0-9]{2}-[0-9]+\.[0-9]{1}$"
-
-# for line in $output
-# do
-# 	if [[ $line =~  $regex ]]
-# 	then
-# 		#is true
-# 		continue
-# 	else
-# 		#failed
-# 		pass="FAILED"
-# 	fi
-# done
-
-# echo "$pass"
-
-
-./lab4b --period=2 --scale=C --log=smoketest_res &
+./lab4b --log=test_output --scale=C --period=1.5 & 
 pid=$!
 sleep 5
 kill $pid
 
-tr " " "_" < smoketest_res > smoketest_result
-rm smoketest_res
-test=$(cat smoketest_result)
-rm smoketest_result
+tr " " "-" < test_output > translated_out
+output=$(cat translated_out)
+rm -rf test_output
+rm -rf translated_out
 
-result="Passes smoketest"
-expected="[0-9]{2}:[0-9]{2}:[0-9]{2}_[0-9]+\.[0-9]$"
-for line in $test
+pass="PASS"
+#regex for each line
+#08:39:17 71.1
+regex="^[0-9]{2}:[0-9]{2}:[0-9]{2}-[0-9]+\.[0-9]{1}$"
+
+for line in $output
 do
-	if [[ $line =~ $expected ]]
+	if [[ $line =~  $regex ]]
 	then
+		#is true
 		continue
 	else
-		result="Fails smoketest"
+		#failed
+		pass="FAILED"
 	fi
 done
 
-echo "$result"
+echo "$pass"
