a4c15e22fbb98256e8e29aac05fe4df2dbdc251d
William Randall <wrandall1000@gmail.com>
2020-11-28 23:56:48 -0600

    update lab4b.c and sources

--- 4b/files/lab4b.c
+++ 4b/files/lab4b.c
@@ -17,10 +17,6 @@
 const int TEMP_PIN = 1;
 const int BUTTON_PIN  = 60;
 
-//set up constants for temp reading
-const int B = 4275;		// B value of the thermistor
-const int R0 = 100000;	// R0 = 100k
-
 //GLOBAL STRUCT
 static struct option arr_option[] =
 {
@@ -37,12 +33,21 @@ static struct option arr_option[] =
 int p_flag = 0;
 int s_flag = 0;
 int l_flag = 0;
+int run_flag = 1;
+int shutdown_flag = 0;
 
 //argument variables
 int p_arg 		= 1;
 char s_arg		= 'F';
 char * l_arg	= NULL;
 int logfd 		= -1;
+
+//buffers
+char read_buf[256] = {0};
+int read_length = 0;
+
+//helper functions
+double get_temp(int temp_reading, char scale);
 
 //stdin arguments
 /**
@@ -97,21 +102,103 @@ int main(int argc, char ** argv){
 
 	//set up log file
 	if(l_flag && l_arg != NULL){
-		if((logfd = (l_arg, O_WRONLY | O_APPEND | O_CREAT, 0666)) < 0){
+		logfd = (l_arg, O_WRONLY | O_APPEND | O_CREAT, 0666);
+		if(logfd < 0){
 			fprintf(stderr, "error on log file\n");
 			exit(2);
 		}
 	}
 
-	//set up sensor
-	mraa_aio_context sensor = mraa_aio_init(TEMP_PIN);
-	mraa_gpio_dir(sensor, MRAA_GPIO_IN);
+	//sensor initialization
+	mraa_aio_context sensor;
+	sensor = mraa_aio_init(TEMP_PIN);
+	if(sensor == NULL){
+		mraa_deinit();
+		fprintf(stderr, "temperature sensor failed to initialize\n");
+		exit(3);
+	}
 
-	//set up button
-	mraa_gpio_context button = mraa_gpio_init(BUTTON_PIN);
-	mraa_gpio_dir(button, MRAA_GPIO_IN);
+	//button initialization
+	mraa_gpio_context button;
+	button = mraa_gpio_init(BUTTON_PIN);
+	if(button == NULL){
+		mraa_deinit();
+		fprintf(stderr, "button failed to initialize\n");
+		exit(3);
+	}
+	if(mraa_gpio_dir(button, MRAA_GPIO_IN) != MRAA_SUCCESS){ // set button dir
+		fprintf(stderr, "button failed to set direction\n");
+		exit(3);
+	}
 
-	
+	//get initial temp
+	int temp_reading = mraa_aio_read(sensor);
+	double temperature = get_temp(temp_reading, s_arg);
+
+	//time variables
+	time_t raw_time;
+	struct tm last_time, current_time;
+	int time_passed = 0; // check if time has passed since last loop
+
+	//start time
+	time(& raw_time);
+	current_time = * localtime(& raw_time);
+
+	//print first temperature reading
+	print_and_log(current_time.tm_hour, current_time.tm_min, current_time.tm_sec, temperature);
+	unsigned int previous_seconds = current_time.tm_sec;
+	last_time = current_time;
+
+	//poll
+	struct pollfd poll_arr[1];
+	poll_arr[0].fd = 0;
+	poll_arr[0].events = POLLIN;
+
+	while(!shutdown_flag){
+		poll(poll_arr, 1, 0);
+
+		//update temperature
+		temp_reading = mraa_aio_read(sensor);
+		temperature = get_temp(temp_reading, s_arg);
+
+		//update time
+		time(& raw_time);
+		current_time = * localtime(& raw_time);
+		int time_now = current_time.tm_min * 60 + current_time.tm_sec
+		int time_prev = last_time.tm_min * 60 + last_time.tm_sec
+		time_passed = time_now - time_prev;
+
+		//print if correct conditions
+		int can_print = run_flag && time_passed > 0 && time_passed % p_arg == 0 && current_time.tm_sec != previous_seconds;
+		if(can_print){
+			print_and_log(current_time.tm_hour, current_time.tm_min, current_time.tm_sec, temperature);
+			previous_seconds = current_time.tm_sec;
+			last_time = current_time;
+		}
+
+		//poll for input
+		if((poll_arr[0].revents & POLLIN) == POLLIN){
+			//read from STDIN
+			read_length = read(STDIN_FILENO, read_buf, 256);
+
+			//loop through each char in the read buffer
+			for(int i = 0; i < read_length; i++){
+
+			}
+		}
+
+		//if button was pressed then stop polling
+		if(mraa_gpio_read(button)){
+			shutdown_flag = 1;
+		}
+	}
+
+	//get final time
+	time(& raw_time);
+	current_time = * localtime(& raw_time);
+
+	//print shutdown
+	print_and_log(current_time.tm_hour, current_time.tm_min, current_time.tm_sec, -1.0);
 
 	//close sensor
 	if(mraa_aio_close(sensor) != MRAA_SUCCESS){
@@ -126,13 +213,44 @@ int main(int argc, char ** argv){
 	return 0;
 }
 
-{
-	//read
-	int temp_reading = mraa_aio_read(sensor);
+double get_temp(int temp_reading, char scale){
+	//set up constants for temp reading
+	const int B = 4275;		// B value of the thermistor
+	const double R0 = 100000;	// R0 = 100k
 
-	float R = 1023.0/temp_reading-1.0;
+	//convert to C or F
+	double R = (1023.0/ (double) temp_reading) - 1.0;
 	R *= R0;
-	float temperature_c = 1.0/(log(R/R0)/B+1.0/298.15)-273.15; // convert to temperature via datasheet
-	float temperature_f = temperature_c * (9.0/5.0) + 32.0;	//convert C to F
+
+	// convert to temperature via datasheet
+	double temperature_c = 1.0 / (log(R / R0) / B + 1.0 / 298.15) - 273.15;
+
+	//convert C to F
+	double temperature_f = temperature_c * (9.0/5.0) + 32.0;
+
+	//output one or the other
+	if(scale == 'C'){
+		return temperature_c;
+	}
+	else if(scale == 'F'){
+		return temperature_f;
+	}
+	else{
+		fprintf(stderr, "invalid use of scale\n");
+		exit(1);
+		return -1;
+	}
 }
 
+void print_and_log(int hour, int min, int sec, double temperature){
+	if(shutdown_flag){
+		printf("%02d:%02d:%02d SHUTDOWN\n", hour, min, sec);
+		if(l_flag)
+			dprintf(log, "%02d:%02d:%02d SHUTDOWN\n", hour, min, sec);
+	}
+	else{
+		printf("%02d:%02d:%02d %.1f\n", hour, min, sec, temperature);
+		if(l_flag)
+			dprintf(log, "%02d:%02d:%02d %.1f\n", hour, min, sec, temperature);
+	}
+}
