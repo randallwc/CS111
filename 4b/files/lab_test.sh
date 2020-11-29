4d5eef5877013fa9e8b03a6f0b0a1a6a9f96cc66
William Randall <wrandall1000@gmail.com>
2020-11-29 03:31:00 -0600

    Update lab_test.sh

--- 4b/files/lab_test.sh
+++ 4b/files/lab_test.sh
@@ -5,13 +5,15 @@ pid=$!
 sleep 10
 kill $pid
 
-output=$(cat test_output)
+tr " " "-" < test_output > translated_out
+output=$(cat translated_out)
 rm -rf test_output
+rm -rf translated_out
 
 pass="PASS"
 #regex for each line
 #08:39:17 71.1
-regex="^[0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]+\.[0-9]{1}$"
+regex="^[0-9]{2}:[0-9]{2}:[0-9]{2}-[0-9]+\.[0-9]{1}$"
 
 for line in $output
 do
